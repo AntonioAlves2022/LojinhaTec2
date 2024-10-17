f7dbf9ae8f8450481fbefa8790a9a0dcc68f6c7c
Antonio Alves <antonio.alves@duque.g12.br>
2024-10-17 20:01:19 -0300

    Aula 17/10/2024 - Foi criada a classe de conexão com o banco de dados mysql

---
+++ src/main/java/br/com/antonioalves/lojinha/Conexao.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.antonioalves.lojinha;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author professor
+ */
+public class Conexao {
+    private static final String SERVER_MYSQL = "jdbc:mysql://localhost:3308/lojinha";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String USER = "root";
+    private static final String PASSWORD = "231281";
+    
+    
+    public static Connection getConnection() throws SQLException{
+        
+        
+    
+    }
+    
+    
+    
+    
+}
