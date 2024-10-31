c70b63ec0714ebfd2bd4fcd5bcc48d3137e004b0
Antonio Alves <antonio.alves@duque.g12.br>
2024-10-31 20:21:24 -0300

    Criar Dao para cadastro de Usuarios

---
+++ src/main/java/br/com/antonioalves/lojinha/dao/InterBanco.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.com.antonioalves.lojinha.dao;
+
+/**
+ *
+ * @author Professor
+ */
+public interface InterBanco {
+    public boolean create();
+    public boolean read();
+    public boolean update();
+    public boolean delete();
+    public boolean findAll();
+    public boolean findById(int id);
+}
---
+++ src/main/java/br/com/antonioalves/lojinha/dao/UsuarioDao.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.antonioalves.lojinha.dao;
+
+import br.com.antonioalves.lojinha.entidades.Usuario;
+
+/**
+ *
+ * @author Professor
+ */
+public class UsuarioDao implements InterBanco{
+    //isto é um atributo (variavel da classe)
+    private Usuario usuario;
+    
+    //parametro de método (dentro dos parenteses)
+    public UsuarioDao(Usuario user){
+        this.usuario = user;
+    }
+
+    @Override
+    public boolean create() {
+        boolean sucesso = false;
+        return sucesso;
+    }
+
+    @Override
+    public boolean read() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean update() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean delete() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean findById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
